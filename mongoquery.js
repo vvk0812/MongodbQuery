adeaf955b265ff5fb9f30b39ffe9c4f7efd8ea35
vvk0812 <vvk7899@gmail.com>
2024-10-08 08:38:44 +0530

    delete ommit

--- mongoquery.js
+++ mongoquery.js
@@ -30,8 +30,13 @@ db.query.updateOne({qty:25},{$set:{tags:['blue','red']}});
 //query to find one and update 
 db.query.findOneAndUpdate({qty:200},{$set:{tags:['blue','green']}});
 
+db.query.findOneAndUpdate({_id:ObjectId("670423aed44824e410ee6352")},{$set:{item:"bread"}});
+  
+db.query.findOneAndUpdate({_id:ObjectId("670422e3007d534739c94af9")},{$set:{item:"keypad",tags:["black","yellow"]}});
+  
 //query to updat many document less than operator
 db.query.updateMany({qty:{$lt:30}},{$set:{qty:200}});
+db.query.updateMany({status:'unpack'},{$set:{qty:25}});
 
 //query to updat many document greator than operator
 db.query.updateMany({qty:{$gt:30}},{$set:{qty:200}});
@@ -40,10 +45,9 @@ db.query.updateMany({qty:{$gt:30}},{$set:{qty:200}});
 db.query.find({})
 //qury to find one document from the collection
 db.query.findOne({item:"mousepad"})    
-  
 
-  
-  
-  
-  
-  
+//query to delete single document 
+db.query.deleteOne({_id:ObjectId('670423aed44824e410ee6352')});
+db.query.deleteMany({qty:{$gt: 30}});
+db.query.deleteMany({status: "unpack"});
+db.query.deleteMany({});
